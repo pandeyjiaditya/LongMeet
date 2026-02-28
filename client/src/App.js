cedd3a23833514d14490ca683640a1346f24e73e
Aditya Pandey <pandeyjiaditya.game@gmail.com>
2026-02-28 21:26:36 +0530

    loading screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext";
+import { AuthProvider, useAuth } from "./context/AuthContext";
 import { SocketProvider } from "./context/SocketContext";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -10,43 +10,60 @@ import Meeting from "./pages/Meeting";
 import Profile from "./pages/Profile";
 import NotFound from "./pages/NotFound";
 import PrivateRoute from "./components/common/PrivateRoute";
+import LoadingScreen from "./components/common/LoadingScreen";
+
+function AppRoutes() {
+  const { loading } = useAuth();
+  const [minTime, setMinTime] = React.useState(true);
+
+  React.useEffect(() => {
+    const t = setTimeout(() => setMinTime(false), 1500);
+    return () => clearTimeout(t);
+  }, []);
+
+  if (loading || minTime) return <LoadingScreen />;
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/dashboard"
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/meeting/:meetingId"
+          element={
+            <PrivateRoute>
+              <Meeting />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <PrivateRoute>
+              <Profile />
+            </PrivateRoute>
+          }
+        />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
+}
 
 function App() {
   return (
     <AuthProvider>
       <SocketProvider>
-        <Router>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route
-              path="/dashboard"
-              element={
-                <PrivateRoute>
-                  <Dashboard />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="/meeting/:meetingId"
-              element={
-                <PrivateRoute>
-                  <Meeting />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="/profile"
-              element={
-                <PrivateRoute>
-                  <Profile />
-                </PrivateRoute>
-              }
-            />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </Router>
+        <AppRoutes />
       </SocketProvider>
     </AuthProvider>
   );
